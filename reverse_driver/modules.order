63b9116c8fbc156035528a466067abdf2ca518ff
varsha-santhosh <varshasanthosh@mirafra.com>
2025-07-01 23:09:08 +0530

    July 1 Session

---
+++ reverse_driver/modules.order
@@ -0,0 +1,1 @@
+/home/hp/Documents/practice/july1/reversedriver/sample_driver_reverse.o
---
+++ reverse_driver/sample_driver_reverse.c
@@ -0,0 +1,76 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/fs.h>
+#include<linux/uaccess.h>
+
+#define DEVICE_NAME "simple_char_dev"
+#define BUF_LEN 128
+//static char str[BUF_LEN];
+static int major;
+static char device_buffer[BUF_LEN];
+static int dev_open(struct inode *inode,struct file *file)
+{
+	printk(KERN_INFO "simple_char_dev: Device opened\n");
+	return 0;
+}
+static int dev_release(struct inode *inode,struct file *file)
+{
+	printk(KERN_INFO "simple_char_dev: Device closed\n");
+	return 0;
+}
+static ssize_t dev_read(struct file *file,char __user *user_buf,size_t count,loff_t *ppos)
+{
+	int bytes_read= strlen(device_buffer);
+	if(copy_to_user(user_buf,device_buffer,bytes_read)){
+		return -EFAULT;
+	}
+	printk(KERN_INFO "simple_char_dev: Read %d bytes\n",bytes_read);
+	return bytes_read;
+}
+static ssize_t dev_write(struct file *file,const char __user *user_buf,size_t count,loff_t *ppos)
+{
+	if(count > BUF_LEN-1)
+		count=BUF_LEN-1;
+	if(copy_from_user(device_buffer,user_buf,count)){
+		return -EFAULT;
+	}
+	int len,i;
+	for(len=0;device_buffer[len];len++);
+	for(i=0,len-=1;i<len;i++,len--)
+	{
+		char temp=device_buffer[i];
+		device_buffer[i]=device_buffer[len];
+		device_buffer[len]=temp;
+	}
+	device_buffer[count]='\0';
+	printk(KERN_INFO "simple_char_dev: Written %zu bytes\n",count);
+	return count;
+}
+static struct file_operations fops = {
+	.open=dev_open,
+	.release = dev_release,
+	.read = dev_read,
+	.write = dev_write,
+};
+static int __init simple_char_init(void)
+{
+	major = register_chrdev(0,DEVICE_NAME,&fops);
+	if(major<0)
+	{
+		printk(KERN_ALERT "Failed to register character device\n");
+		return major;
+	}
+	printk(KERN_INFO "simple_char_dev: Registered with major number %d\n",major);
+	return 0;
+}
+static void __exit simple_char_exit(void)
+{
+	unregister_chrdev(major,DEVICE_NAME);
+	printk(KERN_INFO "simple_char_dev: Unregistered device\n");
+}
+module_init(simple_char_init);
+module_exit(simple_char_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("TechDhaba");
+MODULE_DESCRIPTION("Minimal Character Device Driver");
+
