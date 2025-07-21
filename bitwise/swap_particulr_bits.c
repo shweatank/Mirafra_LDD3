278eedeccc72fbe3c03781d53c248178562f69af
Adinarayana173 <adinarayanapondugala@mirafra.com>
2025-07-21 12:37:46 +0530

    Update swap_particulr_bits.c

--- bitwise/swap_particulr_bits.c
+++ bitwise/swap_particulr_bits.c
@@ -7,12 +7,17 @@ unsigned int swapBits(unsigned int number, unsigned int i, unsigned int j) {
     return number;
 }
 int main() {
-    unsigned int num = 0b10110100;  // 180
+    unsigned int num = 0b10010100;  // 180
     unsigned int i = 2;
     unsigned int j = 5;
 
     printf("Original : %#X (%u)\n", num, num);
     unsigned int swapped = swapBits(num, i, j);
     printf("Swapped  : %#X (%u)\n", swapped, swapped);
+    printf("0b");
+    for(int i=7;i>=0;i--)
+    {
+        printf("%d",((swapped>>i)&1));
+    }
 }
 // o/p  : 10100110
