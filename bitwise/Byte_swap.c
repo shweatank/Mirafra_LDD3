ab2cba7d1a6ab74f263bc46e4995a7145a8c5faa
Adinarayana173 <adinarayanapondugala@mirafra.com>
2025-07-21 12:16:41 +0530

    Update Byte_swap.c

--- bitwise/Byte_swap.c
+++ bitwise/Byte_swap.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	unsigned short num=1234;
-	num=(num<<8) |( num>>8);
-	printf("%x\n",num);
+    int num=0x1234;
+	int res=(((num&0xff)<<8) |( (num&0xff00)>>8));
+	printf("%x\n",res);
 
 }
-
